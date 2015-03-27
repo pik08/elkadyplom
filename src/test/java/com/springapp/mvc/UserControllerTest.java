f42f03c069b11965b5bce6cb709290fbe43b465e
Maciej Jagiello <maciej.jagiello@codilime.com>
2015-03-27 22:43:12 +0100

    Add test

---
+++ src/test/java/com/springapp/mvc/UserControllerTest.java
@@ -0,0 +1,14 @@
+package com.springapp.mvc;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Maciej Jagiello (maciej.jagiello@ntti3.com)
+ */
+public class UserControllerTest {
+
+  @org.junit.Test
+  public void testListUsers() throws Exception {
+    assertTrue(true);
+  }
+}
