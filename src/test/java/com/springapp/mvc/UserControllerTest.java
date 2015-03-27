dae005f99c7dd5edb8a709049c56922e621a0527
Maciej Jagiello <maciej.jagiello@codilime.com>
2015-03-28 00:01:47 +0100

    new fancy test

--- src/test/java/com/springapp/mvc/UserControllerTest.java
+++ src/test/java/com/springapp/mvc/UserControllerTest.java
@@ -1,6 +1,8 @@
 package com.springapp.mvc;
 
-import static org.junit.Assert.*;
+import org.springframework.ui.ModelMap;
+
+import static org.mockito.Mockito.*;
 
 /**
  * @author Maciej Jagiello (maciej.jagiello@ntti3.com)
@@ -9,6 +11,14 @@ public class UserControllerTest {
 
   @org.junit.Test
   public void testListUsers() throws Exception {
-    assertTrue(true);
+    UserController userController = new UserController();
+    ModelMap modelMap = mock(ModelMap.class);
+    when(modelMap.addAttribute(any(String.class), any(User.class))).thenReturn(mock(ModelMap.class));
+    try {
+      userController.listUsers(modelMap);
+    } catch (NullPointerException e) {
+      //skip
+    }
+    verify(modelMap.addAttribute(eq("user"), any(User.class)));
   }
 }
